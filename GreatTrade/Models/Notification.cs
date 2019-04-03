adefdcbf775ccd8df86536670616ea066756a6a3
Nicolas Martinez-Juan Manuel Castillo <nicolasmartinez502@gmail.com>
2019-04-03 14:42:40 -0500

    Notification and Alert added in the model data

---
+++ GreatTrade/Models/Notification.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace GreatTrade.Models
+{
+    public class Notification
+    {
+        public int Type { get; set; }
+        public Boolean Checked { get; set; }
+        public String messasge { get; set; }
+    }
+}
